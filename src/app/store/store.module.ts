a0f33e424b75c4bcb970c0cafc960c4ecd0d6ce1
klm75982 <guillermo.rodriguezfernandez@klm.com>
2019-03-20 13:51:54 +0100

    Fixing bug with the Cart problem

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ModelModule } from '../model/model.module';
-import { StoreComponent } from './store.component';
-import { CounterDirective } from './counter.directive';
-import { CartSummaryComponent } from './cartSummary.component';
-import { CartDetailComponent } from './cartDetail.component';
-import { CheckoutComponent } from './checkout.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ConnectionService } from '../model/connection.service';
+import { ModelModule } from '../model/model.module';
+import { CartDetailComponent } from './cartDetail.component';
+import { CartSummaryComponent } from './cartSummary.component';
+import { CheckoutComponent } from './checkout.component';
+import { CounterDirective } from './counter.directive';
+import { StoreComponent } from './store.component';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
   declarations: [StoreComponent, CounterDirective, CartSummaryComponent,
     CartDetailComponent, CheckoutComponent],
-  exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent],
+  providers: [ConnectionService]
 })
 export class StoreModule {
 }
